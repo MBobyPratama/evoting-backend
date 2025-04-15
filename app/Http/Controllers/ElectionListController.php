f4f9e87959b71a881642072b197da62774e3212b
Muhammad Boby Pratama <bobyjakarta4@gmail.com>
2025-04-15 09:02:09 +0700

    fix(candidate): image

--- app/Http/Controllers/ElectionListController.php
+++ app/Http/Controllers/ElectionListController.php
@@ -219,7 +219,7 @@ class ElectionListController extends Controller
     public function activeElection()
     {
         try {
-            $activeElection = ElectionList::where('status', 'active')->first(['id', 'title', 'status']);
+            $activeElection = ElectionList::where('status', 'active')->first(['id', 'title', 'election_date', 'status']);
 
             if (!$activeElection) {
                 return response()->json([
